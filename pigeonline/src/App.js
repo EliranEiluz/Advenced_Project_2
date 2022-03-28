ea8d22ab5bc111b51df32f1f470f4ed41f30d85c
Ben Levi <benlevi7770528@gmail.com>
2022-03-28 18:34:32 +0300

    files

--- pigeonline/src/App.js
+++ pigeonline/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+
+      <div className="Bar">
+      </div>
+      
     </div>
   );
 }
