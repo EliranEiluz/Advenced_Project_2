34c7adf193c99b9eb2701439765f0e5472905e46
Ben Levi <benlevi7770528@gmail.com>
2022-03-29 17:38:03 +0300

    css

--- pigeonline/src/index.js
+++ pigeonline/src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/register' element={<RegisterPage />}></Route>
-    </Routes>
     <LoginPage />
-    </BrowserRouter>
   </React.StrictMode>
 ,
   document.getElementById('root')
