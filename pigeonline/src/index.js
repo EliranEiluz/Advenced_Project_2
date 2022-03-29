8a60d930c6eb3727301d3f7d7265935a77c29524
EliranEiluz <eliranfr1@gmail.com>
2022-03-29 18:11:04 +0300

    finally succses

--- pigeonline/src/index.js
+++ pigeonline/src/index.js
@@ -10,10 +10,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path='/register' element={<RegisterPage />}></Route>
+      <Route exact path='/register' element={<RegisterPage />}></Route>
       <Route path='/' element={<LoginPage />}></Route>
     </Routes>
-    <LoginPage />
     </BrowserRouter>
   </React.StrictMode>
 ,
