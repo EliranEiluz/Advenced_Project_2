36c5ed9b01c9bab139f7cc6fd5f765edc4b15d61
EliranEiluz <eliranfr1@gmail.com>
2022-03-29 09:39:56 +0300

    Make the site work via React

---
+++ pigeonline/src/LoginPage.js
@@ -0,0 +1,40 @@
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div class="container-fluid">
+            <div class="card row">
+                <div class="card-body">
+                    <div class="row" id="picWrapper">
+                          <img src="im4.png" id="logo"></img>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-6 col-lg-2">Username</label>
+                        <div class="col-10">
+                          <input type="text" class="form-control" placeholder="Please enter your username here..."></input>
+                        </div>
+                      </div>
+                      <div class="mb-3 row">
+                        <label class="col-sm-6 col-lg-2">Password</label>
+                        <div class="col-10">
+                          <input type="password" class="form-control" placeholder="Please enter your password here..."></input>
+                        </div>
+                      </div>
+                      <div class="container">
+                          <div class="row">
+                            <div class="col-7">
+                                Not Registered? <a href="#">click here</a> to register.
+                              </div>
+                              <div class="col-5">
+                                <button type="button" id="loginButton" class="btn btn-outline-primary">Login</button>
+                              </div>
+                          </div>
+                      </div>
+                </div>
+              </div>
+        </div>
+
+  );
+}
+
+export default LoginPage;
