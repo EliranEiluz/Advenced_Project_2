045a5bb634cef3c790f80dce174eeb81dc9f1e77
EliranEiluz <eliranfr1@gmail.com>
2022-03-30 13:44:06 +0300

    updates

--- pigeonline/src/RegisterPage.js
+++ pigeonline/src/RegisterPage.js
@@ -7,23 +7,28 @@ function RegisterPage({UsersArray}) {
   const [userValues, setUserValues] = useState({username:'', password:'', validatePass:'',displayName:'', picture:''});
 
   function validateForm() {
-    return true;
+    return validateUserName() && validatePassword() && validatePassword2() && validateDisplayName() && validatePicture();
   }
 
   function validateUserName() {
-
+    return userValues.username > 0 && !UsersArray.find((e) => e.username == userValues.username);
   }
 
   function validateDisplayName() {
-
+    return userValues.displayName > 0
   }
 
   function validatePassword() {
-
+    return userValues.password > 7 
   }
 
-  function validatePicture() {
+  function validatePassword2() {
+    return userValues.password == userValues.validatePass;
+  }
 
+
+  function validatePicture() {
+    return true;
   }
 
   function handleChange(e) {
@@ -48,31 +53,31 @@ function RegisterPage({UsersArray}) {
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Username</label>
           <div className="col-10">
-            <input name="username" type="text" className="form-control" placeholder="Please enter your username here..." onChange={handleChange} />
+            <input name="username" type="text" className={`form-control ${validateUserName() ? "" : ""}`} placeholder="Please enter your username here..." onChange={handleChange} />
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Password</label>
           <div className="col-10">
-            <input name="password" type="password" className="form-control" placeholder="Please enter your password here..." onChange={handleChange}/>
+            <input name="password" type="password" className={`form-control ${validatePassword() ? "" : ""}`} placeholder="Please enter your password here..." onChange={handleChange}/>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Verify password</label>
           <div className="col-10">
-            <input name="validatePass" type="password" className="form-control" placeholder="Please enter your password again..." onChange={handleChange}/>
+            <input name="validatePass" type="password" className={`form-control ${validatePassword2() ? "" : ""}`} placeholder="Please enter your password again..." onChange={handleChange}/>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Display name</label>
           <div className="col-10">
-            <input name="displayName" className="form-control" placeholder="Please enter your display name..." onChange={handleChange}/>
+            <input name="displayName" className={`form-control ${validateDisplayName() ? "" : ""}`} placeholder="Please enter your display name..." onChange={handleChange}/>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Upload picture</label>
           <div className="col-10">
-            <input name="picture" type="file" id="inputGroupFile02" className="form-control" />
+            <input name="picture" type="file" id="inputGroupFile02" className={`form-control ${validatePicture() ? "" : ""}`} />
           </div>
         </div>
         <div className="container">
