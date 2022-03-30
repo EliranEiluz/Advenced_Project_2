ccb2e74790e46ad17fd5e61d78b0f716075ca633
EliranEiluz <eliranfr1@gmail.com>
2022-03-30 20:53:03 +0300

    some changes

--- pigeonline/src/RegisterPage.js
+++ pigeonline/src/RegisterPage.js
@@ -5,20 +5,50 @@ import { useState } from 'react';
 
 function RegisterPage({UsersArray}) {
   const [userValues, setUserValues] = useState({username:'', password:'', validatePass:'',displayName:'', picture:''});
-
+  
   function validateForm() {
+    /*
+    validateUserName();
+    validatePassword();
+    validatePassword2();
+    validateDisplayName();
+    validatePicture();
+    */
     return validateUserName() && validatePassword() && validatePassword2() && validateDisplayName() && validatePicture();
   }
 
   function validateUserName() {
+    let x = document.getElementById("usernameError")
+    if(userValues.username) {
+      if(!UsersArray.find((e) => e.username == userValues.username)) {
+          x.innerHTML = "";
+      }
+    } else {
+        x.innerHTML = "Username must contain at least one character."
+    }
+    
     return userValues.username > 0 && !UsersArray.find((e) => e.username == userValues.username);
   }
 
   function validateDisplayName() {
+    let x = document.getElementById("displayNameError")
+    if(userValues.displayName) {
+        x.innerHTML = "";
+    }
+    else {
+        x.innerHTML = "The Display name must contain at least one character."
+    }
     return userValues.displayName > 0
   }
 
   function validatePassword() {
+    let x = document.getElementById("passwordError")
+    if (userValues.password > 7) {
+      x.innerHTML = "";
+    }
+    else {
+      x.innerHTML = "Password must contain at least 8 characters."
+    }
     return userValues.password > 7 
   }
 
@@ -43,6 +73,41 @@ function RegisterPage({UsersArray}) {
     event.preventDefault();
   }
 
+
+/*
+  function handleInvalid(str,elementName) {
+    var func;
+    switch(elementName) {
+      case 'username':
+        func = validateUserName;
+        break;
+      case 'password':
+        func = validatePassword;
+        break;
+      case 'password2':
+        func = validatePassword2;
+        break;
+      case 'displayName':
+        func = validateDisplayName;
+        break;
+      case 'picture':
+        func = validatePicture;
+        break;
+    }
+    if (!func()) {
+      return (
+        <div>
+          {str}
+        </div>
+      );
+    }
+    else {
+      return(<></>);
+    }
+  }
+*/
+
+
   return (
     <form className="container-fluid" onSubmit={handleSubmit}>
     <div id="cardRegister" className="card row">
@@ -51,33 +116,47 @@ function RegisterPage({UsersArray}) {
           <img src="im4.png" id="logo" />
         </div>
         <div className="mb-3 row">
-          <label className="col-sm-6 col-lg-2">Username</label>
+          <label for="username" className="col-sm-6 col-lg-2">Username</label>
           <div className="col-10">
-            <input name="username" type="text" className={`form-control ${validateUserName() ? "" : ""}`} placeholder="Please enter your username here..." onChange={handleChange} />
+            <input id="username" name="username" type="text" className="form-control" placeholder="Please enter your username here..." onChange={handleChange} />
+            <div id="usernameError" className="error-divs">
+            Username must contain at least one character.
+            </div>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Password</label>
           <div className="col-10">
-            <input name="password" type="password" className={`form-control ${validatePassword() ? "" : ""}`} placeholder="Please enter your password here..." onChange={handleChange}/>
+            <input name="password" type="password" className="form-control" placeholder="Please enter your password here..." onChange={handleChange}/>
+            <div id="passwordError" className="error-divs">
+              Password must contain at least 8 characters.
+            </div>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Verify password</label>
           <div className="col-10">
-            <input name="validatePass" type="password" className={`form-control ${validatePassword2() ? "" : ""}`} placeholder="Please enter your password again..." onChange={handleChange}/>
+            <input name="validatePass" type="password" className="form-control" placeholder="Please enter your password again..." onChange={handleChange}/>
+            <div id="validatePassError" className="error-divs">
+              This field must be equal to the password above.
+            </div>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Display name</label>
           <div className="col-10">
-            <input name="displayName" className={`form-control ${validateDisplayName() ? "" : ""}`} placeholder="Please enter your display name..." onChange={handleChange}/>
+            <input name="displayName" className="form-control" placeholder="Please enter your display name..." onChange={handleChange}/>
+            <div id="displayNameError" className="error-divs">
+              The display name must contain at least one character.
+            </div>
           </div>
         </div>
         <div className="mb-3 row">
           <label className="col-sm-6 col-lg-2">Upload picture</label>
           <div className="col-10">
-            <input name="picture" type="file" id="inputGroupFile02" className={`form-control ${validatePicture() ? "" : ""}`} />
+            <input name="picture" type="file" id="inputGroupFile02" className="form-control" onChange={handleChange}/>
+            <div id="pictureError" className="error-divs">
+            </div>
           </div>
         </div>
         <div className="container">
