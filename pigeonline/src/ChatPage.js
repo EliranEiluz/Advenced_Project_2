d0131de8737320693f9113a17f5698718af77bd3
Ben Levi <benlevi7770528@gmail.com>
2022-03-31 14:47:14 +0300

    chat

--- pigeonline/src/ChatPage.js
+++ pigeonline/src/ChatPage.js
@@ -6,7 +6,7 @@ function ChatPage() {
   return (
     <div className="container-fluid">
     <div className="row" id="Bar">
-      <div className="col"><img src="im4.png" id="logo" /></div>
+      <div className="col"><img src="im4.png" id="leftLogo" /></div>
       <div className="col" style={{paddingRight: 0}}><img src="logo.png" id="webLogo" /></div>
     </div>
     <div id="chatCard" className="card">
