9dcc35452cee5d25df8c90edd883022610786b54
Ben Levi <benlevi7770528@gmail.com>
2022-03-31 22:47:52 +0300

    chat

--- pigeonline/src/ChatPage.js
+++ pigeonline/src/ChatPage.js
@@ -69,18 +69,31 @@ function ChatPage() {
                   </svg>
                 </button>
                 <ul class="dropdown-menu">
-                  <li><button class="dropdown-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
-                      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                      <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z" />
-                    </svg>
-                  </button></li>
+                  <li>
+                      <div class="img-upload dropdown-item">
+                        <label for="file-input">
+                          <svg id="iconImg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
+                            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                            <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z" />
+                          </svg>
+                        </label>
+                        <input id="file-input" type="file" />
+                      </div>
+                  </li>
                   <li><hr class="dropdown-divider"></hr></li>
-                  <li><button class="dropdown-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
+
+                  <li>
+                      <div class="vid-upload dropdown-item">
+                        <label for="file-input">
+                        <svg id="iconVid" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z" />
                     </svg>
-                  </button></li>
+                        </label>
+                        <input id="file-input" type="file" />
+                      </div>
+                  </li>
+
+                  {/*change last li to lable+input*/}
                   <li><hr class="dropdown-divider"></hr></li>
                   <li><button class="dropdown-item">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
