8e560df1733bdd2d67da7cc14def1370dcc26b4b
Ben Levi <benlevi7770528@gmail.com>
2022-04-06 00:51:43 +0300

    chat

--- pigeonline/src/SampleChat.js
+++ pigeonline/src/SampleChat.js
@@ -249,14 +249,14 @@ function SampleChat() {
         </div>
       </div>
       <div className="row type_msg">
-      <div className='col-sm-1 col-xs-1 col' id='sendBut'>
+      <div className='col-xl-1 col-sm-1 col-xs-1 col' id='sendBut'>
       <button className="SendButton msg_send_btn" type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
                     <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
                 </svg>
             </button>
             </div>
-            <div className='col-sm-1 col-xs-1 col' id='dropUpCol'>
+            <div className='col-xl-1 col-sm-1 col-xs-1 col' id='dropUpCol'>
             <div className="btn-group dropup" id='dropUpMenu'>
             <button type="button" id="dropdownMenu2" data-bs-toggle="dropdown">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
@@ -300,7 +300,7 @@ function SampleChat() {
             </ul>
         </div>
             </div>
-            <div className='col-sm-10 col-xs-10 col' id='inputRow'>
+            <div className='col-xl-10 col-sm-10 col-xs-10 col' id='inputRow'>
         <input type="text" className="form-control" id="messageText" placeholder="New message here..."></input>
         </div>
       </div>
