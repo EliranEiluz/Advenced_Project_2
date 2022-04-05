5b67f7b7451055c7ee97a14ac390082937190b09
Ben Levi <benlevi7770528@gmail.com>
2022-04-05 19:58:22 +0300

    chatPage

--- pigeonline/src/SampleChat.js
+++ pigeonline/src/SampleChat.js
@@ -16,15 +16,29 @@ function SampleChat() {
           <h4>Recent</h4>
         </div>
         <div className="srch_bar">
-          <div className="stylish-input-group">
-            <input type="text" className="search-bar" placeholder="Search" />
-            <span className="input-group-addon">
-              <button type="button">
-                {" "}
-                <i className="fa fa-search" aria-hidden="true" />{" "}
-              </button>
-            </span>{" "}
+        <button type="button" id="addUser" data-bs-toggle="modal" data-bs-target="#contactModal">
+        <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="bi bi-person-plus-fill">
+          <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+          <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+        </svg>
+      </button>
+
+        <div className="modal fade" id="contactModal" tabIndex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="ModalLabel">Add new contact</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+              <input type="text" className="form-control" placeholder="Contact's identifier"></input>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">Add</button>
+              </div>
+            </div>
           </div>
+        </div>
         </div>
       </div>
       <div className="inbox_chat">
