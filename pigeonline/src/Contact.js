ab0f2fc278a3343465db2e10cc4dd56ed6971649
EliranEiluz <eliranfr1@gmail.com>
2022-04-09 14:53:45 +0300

    move style to css.

--- pigeonline/src/Contact.js
+++ pigeonline/src/Contact.js
@@ -22,7 +22,7 @@ function Contact({nowOnline, UsersArray})  {
     <>
     <div className="row" id="Bar">
       <div className="col"><img src="im4.png" id="leftLogo" /></div>
-      <div className="col" style={{ paddingRight: 0 }}><img src="logo.png" id="webLogo" /></div>
+      <div className="col logoWrap"><img src="logo.png" id="webLogo" /></div>
     </div>
 
     <div className="messaging">
