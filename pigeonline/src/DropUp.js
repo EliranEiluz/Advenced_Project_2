3eb1816391f2ea4a6e74c721ab5cc0fa96634605
Ben Levi <benlevi7770528@gmail.com>
2022-04-10 21:05:53 +0300

    record modal

--- pigeonline/src/DropUp.js
+++ pigeonline/src/DropUp.js
@@ -63,6 +63,8 @@ function DropUp({newPictureMessage, newVideoMessage}) {
         stop.addEventListener('click', function (ev) {
           mediaRecorder.stop();
           // console.log(mediaRecorder.state);
+          audio.pause();
+          audio.stop();
         });
  
         // If audio data available then push
@@ -77,7 +79,6 @@ function DropUp({newPictureMessage, newVideoMessage}) {
         // Convert the audio data in to blob
         // after stopping the recording
         mediaRecorder.onstop = function (ev) {
- 
           // blob of type mp3
           let audioData = new Blob(dataArray,
                     { 'type': 'audio/mp3;' });
@@ -160,12 +161,22 @@ function DropUp({newPictureMessage, newVideoMessage}) {
           <div className="modal-body">
 
           <p>
-            <button id="btnStart" onClick={startRecord}>START RECORDING</button>
+            <button id="btnStart" onClick={startRecord}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-record-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                </svg>
+            </button>
                   &nbsp;&nbsp;&nbsp;&nbsp;
-            <button id="btnStop">STOP RECORDING</button>
+            <button id="btnStop">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-stop-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z" />
+                </svg>
+            </button>
           </p>
          
-            <audio controls></audio>
+            <audio id="recordPlay" controls></audio>
 
             <audio id="adioPlay" controls></audio>
 
