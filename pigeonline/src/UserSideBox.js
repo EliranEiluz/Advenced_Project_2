b094938a2cc2068390d99037589552c00b389f06
EliranEiluz <eliranfr1@gmail.com>
2022-04-11 17:25:38 +0300

    changes

--- pigeonline/src/UserSideBox.js
+++ pigeonline/src/UserSideBox.js
@@ -1,4 +1,4 @@
-import { Link,useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './ChatPage.css';
 import './SampleChat.css'
 import Message from './Message';
