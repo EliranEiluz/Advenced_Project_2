4715f3a4afda185043796800d18926626c8befe6
Ben Levi <benlevi7770528@gmail.com>
2022-04-21 16:47:18 +0300

    send message with press enter.

--- pigeonline/src/ChatWindow.js
+++ pigeonline/src/ChatWindow.js
@@ -236,7 +236,15 @@ function ChatWindow({setChats, nowOnline, chatMessages, contactUserName, UsersAr
         const date = hours + ":" + minutes + " | " + days + "/" + months + "/" + currentDate.getFullYear();
         return date;
     }
+    
 
+    function handleKeyDown(event) {
+      if(event.key === "Enter") { 
+        document.getElementById("buttonForSend").click();
+      }
+    }
+    
+    
   return (
     <div className="mesgs">
       <div className="msg_history">
@@ -246,7 +254,7 @@ function ChatWindow({setChats, nowOnline, chatMessages, contactUserName, UsersAr
       <div className="row type_msg">
       
         <div className='col-xl-1 col-sm-1 col-xs-1 col' id='sendBut'>
-          <button className="SendButton msg_send_btn" type="button" onClick={newTextMessage}>
+          <button id="buttonForSend" className="SendButton msg_send_btn" type="button" onClick={newTextMessage}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
               <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
             </svg>
@@ -256,7 +264,7 @@ function ChatWindow({setChats, nowOnline, chatMessages, contactUserName, UsersAr
         <DropUp newPictureMessage={newPictureMessage} newVideoMessage={newVideoMessage} startRecord={startRecord} newRecordMessage={newRecordMessage} resetRecord={resetRecord} />
 
         <div className='col-xl-10 col-sm-10 col-xs-10 col' id='inputRow'>
-          <input type="text" className="form-control" id="messageText" placeholder="New message here..." onChange={handleChange}></input>
+          <input type="text" className="form-control" id="messageText" placeholder="New message here..." onChange={handleChange} onKeyDown={handleKeyDown}></input>
         </div>
 
       </div>
@@ -265,4 +273,3 @@ function ChatWindow({setChats, nowOnline, chatMessages, contactUserName, UsersAr
 }
 
 export default ChatWindow;
-
